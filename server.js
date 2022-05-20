b4ba7a3ada2bf7889676b4d8739b593f94cca42b
Dylan Duke <laytonmaes@gmail.com>
2022-05-20 15:43:45 -0600

    Add get and post data

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express()
 
+app.use(express.json());
+
 app.set("port", process.env.PORT || 3001)
 app.locals.title = "Rancid Tomatillo Data"
-app.locals.favorites = [{movie: "Iron Giant"}]
+
+app.locals.favorites = [
+        {
+        "id": 694919,
+        "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
+        "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
+        "title": "Money Plane",
+        "average_rating": 6.875,
+        "release_date": "2020-09-29"
+        }
+    ]
 
 
 
@@ -11,10 +23,19 @@ app.get("/", (request, response) => {
     response.send("Rancid Tomatillos");
 });
 
-app.get("/api/v1/pets", (request, response) => {
+app.get("/api/v1/user/favorites", (request, response) => {
     const favorites = app.locals.favorites;
 
     response.json({ favorites })
+})
+
+app.post("/api/v1/user/favorites", (request, response) => {
+    console.log(request.body)
+    const {  id, title, poster_path, backdrop_path, average_rating, release_date } = request.body
+
+    app.locals.favorites.push({  id, title, poster_path, backdrop_path, average_rating, release_date })
+
+    response.status(201).json({  id, title, poster_path, backdrop_path, average_rating, release_date })
 })
 
 app.listen(app.get("port"), () => {
